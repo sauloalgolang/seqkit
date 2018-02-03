72b84497a75c5cfa254a924e24aaa6672d9735e8
Saulo Aflitos <sauloal@gmail.com>
2018-02-03 20:50:40 +0100

    logging; tests; passing basic tests

--- seqkit/cmd/kmerLib.go
+++ seqkit/cmd/kmerLib.go
@@ -3,10 +3,19 @@ package cmd
 import (
 	"sort"
 	"golang.org/x/text/message"
+	//"github.com/op/go-logging"
+	//"github.com/shenwei356/go-logging"
 )
 
-var p = message.NewPrinter(message.MatchLanguage("en"))
+//https://github.com/op/go-logging
 
+//var log = logging.MustGetLogger("Kmer")
+
+//var format = logging.MustStringFormatter(
+//	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+//)
+
+var p = message.NewPrinter(message.MatchLanguage("en"))
 
 
 type KmerUnit struct {
@@ -17,83 +26,175 @@ type KmerUnit struct {
 type KmerArr []KmerUnit
 
 func (this *KmerArr) Print() {
-	//p.Printf( "kmerSize  %12d\n", kmerSize    )
+	//log.Debugf(p.Sprintf( "kmerSize  %12d\n", kmerSize    )
 	for i,j := range *this {
-		p.Printf( "  %12d :: %12d -> %3d\n", i, j.Kmer, j.Count )
+		log.Debugf(p.Sprintf( "  %12d :: %12d -> %3d\n", i, j.Kmer, j.Count ))
 	}
 }
 
 func (this *KmerArr) Sort() {
+	log.Debugf("KmerArr    :: Sort", (*this))
+	(*this).Print()
 	//https://stackoverflow.com/questions/28999735/what-is-the-shortest-way-to-simply-sort-an-array-of-structs-by-arbitrary-field
+
 	sort.Slice((*this)[:], func(i, j int) bool {
 		return (*this)[i].Kmer < (*this)[j].Kmer
 	})
+	
+	log.Debugf("KmerArr    :: Sort", (*this))
+	(*this).Print()
 }
 
 func (this *KmerArr) Merge(that *KmerArr) {
-	lenThis   :=           len((*this))
-	lenThat   := lenThis + len((*that))
+	log.Debugf("KmerArr    :: Merge :: THIS: ", (*this), len((*this)), cap((*this)))
+	log.Debugf("KmerArr    :: Merge :: THAT: ", (*that), len((*that)), cap((*that)))
+
+	lenThis   := len((*this))
+	lenThat   := len((*that))
+	lenAll    := lenThis + lenThat
 
 	indexThis := 0
-	indexThat := lenThis
+	indexThat := 0
 
-	(*this)    = append((*this), (*that)...)
-	
-	var thisKmer  *KmerUnit
-	var thatKmer  *KmerUnit
-	
-	var thisKmerK *uint64
-	var thatKmerK *uint64
-	
-	var thisKmerC *uint8
-	var thatKmerC *uint8
-	
-	for {
-		if indexThat == lenThat-1 {
-			break
-		}
+	if lenThis == 0 {
+		log.Debugf("KmerArr    :: Merge :: Copy :: ", len((*this)), cap((*this)))
+		(*this) = make(KmerArr, len((*that)), len((*that)))
+		copy((*this), (*that))
 		
-		thisKmer  = &(*this)[indexThis]
-		thatKmer  = &(*that)[indexThat]
+	} else {
+		log.Debugf("KmerArr    :: Merge :: Merge & Sort :: ", len((*this)), cap((*this)))
+
+		(*this) = append((*this), make(KmerArr, lenAll, lenAll)...)
 		
-		thisKmerK = &thisKmer.Kmer
-		thatKmerK = &thatKmer.Kmer
+		var thisKmer  *KmerUnit
+		var thatKmer  *KmerUnit
 		
-		thisKmerC = &thisKmer.Count
-		thatKmerC = &thatKmer.Count
+		var thisKmerK *uint64
+		var thatKmerK *uint64
 		
-		if *thisKmerK == *thatKmerK {
-			if *thisKmerC < 254 {
-				if uint64(*thisKmerC) + uint64(*thatKmerC) > 254 {
-					*thisKmerC  = 254
-				} else {
-					*thisKmerC += *thatKmerC
+		var thisKmerC *uint8
+		var thatKmerC *uint8
+				
+		for {
+			log.Debugf("")
+			log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThis : ",   indexThis )
+			log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat : ",   indexThat )
+			if indexThis == lenThis { // no more this
+				log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThis == lenThis" )
+				if indexThat == lenThat { // no more that. should be fully sorted
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat == lenThat. breaking" )
+					break
+				} else { // still that. append
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat != lenThat. appending" )
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat != lenThat. appending :: indexThis: ", indexThis )
+					for ;indexThat<lenThat; indexThat++ {
+						(*this)[indexThis] = (*that)[indexThat]
+						indexThis++
+						log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat != lenThat. appending :: indexThis: ", indexThis, " indexThat: ", indexThat )
+					}
+					break
+				}
+			} else { // still has this
+				log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThis != lenThis" )
+				if indexThat == lenThat { // no more that. should be fully sorted
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat == lenThat. breaking" )
+					break
+				
+				} else { // still this and still that. keep sorting
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat != lenThat. merging" )
+					
+					thisKmer  = &(*this)[indexThis]
+					thatKmer  = &(*that)[indexThat]
+					
+					thisKmerK = &thisKmer.Kmer
+					thatKmerK = &thatKmer.Kmer
+					
+					thisKmerC = &thisKmer.Count
+					thatKmerC = &thatKmer.Count
+					
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: thisKmerK : ", (*thisKmerK))
+					log.Debugf("KmerArr    :: Merge :: Merge & Sort :: thatKmerK : ", (*thatKmerK))
+				
+					if *thisKmerK == *thatKmerK {
+						if *thisKmerC < 254 {
+							if uint64(*thisKmerC) + uint64(*thatKmerC) > 254 {
+								*thisKmerC  = 254
+							} else {
+								*thisKmerC += *thatKmerC
+							}
+						}
+						
+						indexThis++
+						indexThat++
+						
+					} else if *thisKmerK < *thatKmerK {
+						indexThis++
+						
+					} else if *thisKmerK > *thatKmerK {
+						(*this)[indexThis], (*that)[indexThat] = (*that)[indexThat], (*this)[indexThis]
+						indexThatK := indexThat
+						for {
+							if indexThatK < lenThat-1 && (*that)[indexThatK].Kmer > (*that)[indexThatK+1].Kmer {
+								(*that)[indexThatK], (*that)[indexThatK+1] = (*that)[indexThatK+1], (*that)[indexThatK]
+								indexThatK++
+							} else {
+								break
+							}
+						}
+						//indexThis++
+					}
+					log.Debugf( "this" )
+					this.Print()
+					log.Debugf( "that" )
+					that.Print()
 				}
 			}
-			
-			indexThis++
-			indexThat++
-			
-		} else if *thisKmerK < *thatKmerK {
-			indexThis++
-			
-		} else if *thisKmerK > *thatKmerK {
-			(*this)[indexThis], (*this)[indexThat] = (*that)[indexThat], (*that)[indexThis]
-			indexThat++
 		}
+	
+		log.Debugf("KmerArr    :: Merge :: Merge & Sort :: lenThis   : ",   lenThis)
+		log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThis : ", indexThis)
+		log.Debugf("KmerArr    :: Merge :: Merge & Sort :: indexThat : ", indexThat)
+		boundaryThis := indexThis
+		if indexThis < lenThis {
+			boundaryThis = lenThis
+		}
+	
+		(*this) = (*this)[:boundaryThis]
+		log.Debugf("KmerArr    :: Merge :: Merge & Sort :: ", len((*this)), cap((*this)))
 	}
 	
-	(*this) = (*this)[:indexThis]
+	(*that).Clear()
+	
+	log.Debugf("KmerArr    :: Merge :: THIS: ", (*this), len((*this)), cap((*this)))
+	log.Debugf("KmerArr    :: Merge :: THAT: ", (*that), len((*that)), cap((*that)))
 }
 
 func (this *KmerArr) Add(kmer uint64) {
-	println("KmerArr    :: Add", kmer, (*this))
+	log.Debugf("KmerArr    :: Add", kmer, (*this))
 	(*this) = append((*this), KmerUnit{kmer, 1})
-	println("KmerArr    :: Add", kmer, (*this))
+	log.Debugf("KmerArr    :: Add", kmer, (*this))
 }
 
-func (this *KmerArr) Clean() {
+func (this *KmerArr) Clear() {
+	log.Debugf("KmerArr    :: Clear", (*this), len((*this)), cap((*this)))
+	(*this) = (*this)[:0]
+	log.Debugf("KmerArr    :: Clear", (*this), len((*this)), cap((*this)))
+}
+
+func (this *KmerArr) isEqual(that *KmerArr) (bool, string) {
+	if len(*this) != len(*that) {
+		return false, "Sizes differ"
+	}
 	
+	for i,j := range (*this) {
+		if j.Kmer != (*that)[i].Kmer {
+			return false, "Kmer out of order"
+		}
+		if j.Count != (*that)[i].Count {
+			return false, "Kmer count differ"
+		}
+	}	
+	return true, "OK"
 }
 
 
@@ -110,36 +211,35 @@ type KmerHolder struct {
 }
 
 func (this *KmerHolder) Print() {	
-	p.Printf( "kmerSize     %12d\n", this.KmerSize  )
-	p.Printf( "KmerLen      %12d\n", this.KmerLen   )
-	p.Printf( "BufferLen    %12d\n", this.BufferLen )
-	p.Printf( "KmerCap      %12d\n", this.KmerCap   )
-	p.Printf( "BufferCap    %12d\n", this.BufferCap )
+	log.Debugf(p.Sprintf( "kmerSize     %12d\n", this.KmerSize  ))
+	log.Debugf(p.Sprintf( "KmerLen      %12d\n", this.KmerLen   ))
+	log.Debugf(p.Sprintf( "BufferLen    %12d\n", this.BufferLen ))
+	log.Debugf(p.Sprintf( "KmerCap      %12d\n", this.KmerCap   ))
+	log.Debugf(p.Sprintf( "BufferCap    %12d\n", this.BufferCap ))
 
-	p.Printf( "Kmer         %12d CAP %12d\n", len(this.Kmer  ), cap(this.Kmer  ) )
+	log.Debugf(p.Sprintf( "Kmer         %12d CAP %12d\n", len(this.Kmer  ), cap(this.Kmer  ) ))
 	this.Kmer.Print()
 	
-	p.Printf( "Buffer       %12d CAP %12d\n", len(this.Buffer), cap(this.Buffer) )
+	log.Debugf(p.Sprintf( "Buffer       %12d CAP %12d\n", len(this.Buffer), cap(this.Buffer) ))
 	this.Buffer.Print()
 }
 
 func (this *KmerHolder) Sort() {
-	println("KmerHolder :: Sort")
+	log.Debugf("KmerHolder :: Sort")
 	this.Buffer.Sort()
 	this.Kmer.Merge(&this.Buffer)
-	this.Buffer.Clean()
 	this.KmerLen = len(this.Kmer)
 	this.BufferLen = len(this.Buffer)
 }
 
 func (this *KmerHolder) Add(kmer uint64) {
-	println("KmerHolder :: Add", kmer, this.Buffer)
+	log.Debugf("KmerHolder :: Add", kmer, this.Buffer)
 	if this.BufferLen == this.BufferCap {
 		this.Sort()
 	}
-	println("KmerHolder :: Add", kmer, this.Buffer)
+	log.Debugf("KmerHolder :: Add", kmer, this.Buffer)
 	this.Buffer.Add(kmer)
-	println("KmerHolder :: Add", kmer, this.Buffer)
+	log.Debugf("KmerHolder :: Add", kmer, this.Buffer)
 	this.BufferLen = len(this.Buffer)
 }
 
@@ -163,5 +263,6 @@ func NewKmerHolder(kmerSize uint64) *KmerHolder {
 	k.BufferCap      =     1000
 	k.Kmer           = make(KmerArr, 0, k.KmerCap  )
 	k.Buffer         = make(KmerArr, 0, k.BufferCap)
+
 	return &k
 }
--- seqkit/cmd/kmerLib_test.go
+++ seqkit/cmd/kmerLib_test.go
@@ -4,27 +4,50 @@ package cmd
 
 import (
         "testing"
+        "github.com/shenwei356/go-logging"
 )
 
 func TestSort(t *testing.T) {
-    var kmerSize uint64 = 2
+    testList(t, 2, 3, 2, []uint64{1,2,3,4,5}            , KmerArr{{1,1},{2,1},{3,1},{4,1},{5,1}})
+    testList(t, 2, 3, 2, []uint64{1,2,3,4,5,6,7,8,9,10} , KmerArr{{1,1},{2,1},{3,1},{4,1},{5,1},{6,1},{7,1},{8,1},{9,1},{10,1}})
+    testList(t, 2, 3, 2, []uint64{1,2,3,4,5,1,2,3,4,5}  , KmerArr{{1,2},{2,2},{3,2},{4,2},{5,2}})
+    testList(t, 2, 3, 2, []uint64{1,2,3,4,5,1,2,3,4,5,6}, KmerArr{{1,2},{2,2},{3,2},{4,2},{5,2},{6,1}})
+    testList(t, 2, 3, 2, []uint64{1,6,2,5,3,3}          , KmerArr{{1,1},{2,1},{3,2},{5,1},{6,1}})
+    testList(t, 2, 3, 2, []uint64{1,6,5,2,3,3,0}        , KmerArr{{0,1},{1,1},{2,1},{3,2},{5,1},{6,1}})
+ 
+}
+
+func testList(t *testing.T, kmerSize uint64, KmerCap int, BufferCap int, list []uint64, check KmerArr) {
+    logging.SetLevel(logging.INFO, "seqkit")
+    
     var res       = NewKmerHolder(kmerSize)
 	res.KmerSize  = kmerSize
-	res.KmerCap   =       3
-	res.BufferCap =       2
+	res.KmerCap   = KmerCap
+	res.BufferCap = BufferCap
 	res.Kmer      = make(KmerArr, 0, res.KmerCap  )
 	res.Buffer    = make(KmerArr, 0, res.BufferCap)
  
-    var list []uint64 = []uint64{1,2,3,4,5,6,7,8,9,10}
- 
-    println("before")
+ 	log.Debug("before")
     res.Print()
-    println()
+    log.Debug("")
     for _,j := range list {
-        println("adding", j)
+        log.Debug("adding", j)
         res.Add(j)
         res.Print()
-        println()
+        log.Debug("")
     }
     res.Close()
+    res.Print()
+    
+    status, msg := res.Kmer.isEqual(&check)
+    
+    if status {
+        t.Log("For", list, "- OK")
+    } else {
+        t.Error(
+                "For"     , list    ,
+                "Expected", check   ,
+                "Got"     , res.Kmer,
+                "Error"   , msg)
+    }
 }
