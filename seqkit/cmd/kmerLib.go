023708c0586693c064406854de32a7510ac98ebf
Saulo Aflitos <sauloal@gmail.com>
2018-02-15 23:59:32 +0100

    using struct for export/inport statistics and check

--- seqkit/cmd/kmerLib.go
+++ seqkit/cmd/kmerLib.go
@@ -244,6 +244,22 @@ func (this *KmerIO) WriteUint64V(x uint64) {
 	}
 }
 
+func (this *KmerIO) WriteStruct(x interface{}) {
+	this.CheckMode(1)
+	err := binary.Write(this.OutFh, binary.LittleEndian, x)
+	if err != nil {
+		log.Panic("binary.Write failed:", err)
+	}
+}
+func (this *KmerIO) ReadStruct(x interface{}) {
+	this.CheckMode(2)
+	err := binary.Read(this.InFh, binary.LittleEndian, x)
+	if err != nil {
+		log.Panic("binary.Write failed:", err)
+	}
+}
+
+
 func (this *KmerIO) Flush() {
 	this.CheckMode(1)
 
@@ -817,6 +833,66 @@ func (this *KmerHolder) ToFile(outFile string, minCount uint8) bool {
 	return this.ToFileHandle(outFh, minCount)
 }
 
+type ChecksumK struct {
+	NumK     uint64
+	MinK     uint64
+	MaxK     uint64
+	MinC     uint8
+	MaxC     uint8
+	MinD     uint64
+	MaxD     uint64
+	SumC     uint64
+	SumD     uint64
+}
+
+func (this *ChecksumK) Add(kmer uint64, count uint8, kmerdiff uint64) {
+	this.NumK++
+	
+	if kmer     < this.MinK { this.MinK = kmer     }
+	if kmer     > this.MaxK { this.MaxK = kmer     }
+	if count    < this.MinC { this.MinC = count    }
+	if count    > this.MaxC { this.MaxC = count    }
+	if kmerdiff < this.MinD { this.MinD = kmerdiff }
+	if kmerdiff > this.MaxD { this.MaxD = kmerdiff }
+	
+	this.SumC += uint64(count)
+	this.SumD += kmerdiff
+}
+
+
+func (this *ChecksumK) Print() {
+	fmt.Printf("valid: %12d :: kmer min: %12d max: %12d :: diff sum: %12d min: %12d max: %12d :: count sum: %12d min: %12d max: %12d\n",
+			   this.NumK, this.MinK, this.MaxK, this.SumD, this.MinD, this.MaxD, this.SumC, this.MinC, this.MaxC )
+}
+
+func (this *ChecksumK) IsEqual(that *ChecksumK) {
+	if this.NumK != that.NumK { log.Panicf("number of kmer not the same as expected. %d vs %d"   , this.NumK, that.NumK) }
+	if this.MinK != that.MinK { log.Panicf("minimal kmer not the same as expected. %d vs %d"     , this.MinK, that.MinK) }
+	if this.MaxK != that.MaxK { log.Panicf("maximum kmer not the same as expected. %d vs %d"     , this.MaxK, that.MaxK) }
+	if this.MinC != that.MinC { log.Panicf("minimal count not the same as expected. %d vs %d"    , this.MinC, that.MinC) }
+	if this.MaxC != that.MaxC { log.Panicf("maximum count not the same as expected. %d vs %d"    , this.MaxC, that.MaxC) }
+	if this.MinD != that.MinD { log.Panicf("minimal kmer diff not the same as expected. %d vs %d", this.MinD, that.MinD) }
+	if this.MaxD != that.MaxD { log.Panicf("maximum kmer diff not the same as expected. %d vs %d", this.MaxD, that.MaxD) }
+	if this.SumC != that.SumC { log.Panicf("sum of counts not the same as expected. %d vs %d"    , this.SumC, that.SumC) }
+	if this.SumD != that.SumD { log.Panicf("sum of diff not the same as expected. %d vs %d"      , this.SumD, that.SumD) }
+}
+			   
+func NewChecksumK() *ChecksumK {
+	csk       := ChecksumK{}
+
+	csk.NumK   = 0
+	csk.MinK   = MaxUint
+	csk.MaxK   = 0
+	csk.MinC   = 254
+	csk.MaxC   = 0
+	csk.MinD   = MaxUint
+	csk.MaxD   = 0
+	csk.SumC   = 0
+	csk.SumD   = 0
+	
+	return &csk
+}
+
 func (this *KmerHolder) ToFileHandle(outFh *xopen.Writer, minCount uint8) bool {
 	println("saving to stream")
 	
@@ -829,15 +905,7 @@ func (this *KmerHolder) ToFileHandle(outFh *xopen.Writer, minCount uint8) bool {
 	var kmerdiff uint64 = 0
 	
 	var numK     uint64 = 0
-	var numV     uint64 = 0
-	var minK     uint64 = MaxUint
-	var maxK     uint64 = 0
-	var minC     uint8  = 254
-	var maxC     uint8  = 0
-	var minD     uint64 = MaxUint
-	var maxD     uint64 = 0
-	var sumC     uint64 = 0
-	var sumD     uint64 = 0
+	csk := NewChecksumK()
 
 	var regs     uint64 = uint64(len(this.Kmer))
 	fmt.Printf("writing %12d registers\n", regs)
@@ -858,18 +926,10 @@ func (this *KmerHolder) ToFileHandle(outFh *xopen.Writer, minCount uint8) bool {
 			continue
 		}
 
-		numV++
 		kmerdiff    = kmer - lastKmer
-		if kmer     < minK { minK = kmer     }
-		if kmer     > maxK { maxK = kmer     }
-		if count    < minC { minC = count    }
-		if count    > maxC { maxC = count    }
-		if kmerdiff < minD { minD = kmerdiff }
-		if kmerdiff > maxD { maxD = kmerdiff }
 		
-		sumC += uint64(count)
-		sumD += kmerdiff
-		
+		csk.Add(kmer, count, kmerdiff)
+				
 		//fmt.Printf("W k %d kmer %d count %d kmerdiff %d\n", k, kmer, count, kmerdiff)
 		kio.WriteUint64(kmerdiff)
 		//kio.WriteUint64V(kmerdiff)
@@ -877,18 +937,11 @@ func (this *KmerHolder) ToFileHandle(outFh *xopen.Writer, minCount uint8) bool {
 		lastKmer = kmer
 	}
 
-	fmt.Printf("WRITE registers: %12d valid: %12d :: kmer min: %12d max: %12d :: diff sum: %12d min: %12d max: %12d :: count sum: %12d min: %12d max: %12d\n",
-			   numK, numV, minK, maxK, sumD, minD, maxD, sumC, minC, maxC )
+	fmt.Printf("WRITE registers: %12d ", numK )
+	
+	csk.Print()
 
-	kio.WriteUint64(numV)
-	kio.WriteUint64(minK)
-	kio.WriteUint64(maxK)
-	kio.WriteUint8 (minC)
-	kio.WriteUint8 (maxC)
-	kio.WriteUint64(minD)
-	kio.WriteUint64(maxD)
-	kio.WriteUint64(sumC)
-	kio.WriteUint64(sumD)
+	kio.WriteStruct(csk)
 
 	if numK != regs {
 		log.Panicf("number of writen registers not the same as expected. %d vs %d", numK, regs)
@@ -911,6 +964,7 @@ func (this *KmerHolder) FromFileHandle(inFh *xopen.Reader) bool {
 
 	kio := KmerIO{}
 	kio.initReader(inFh)
+	csk := NewChecksumK()
 
 	var kmer     uint64 = 0
 	var count    uint8  = 0
@@ -922,15 +976,6 @@ func (this *KmerHolder) FromFileHandle(inFh *xopen.Reader) bool {
 	var minCount uint8  = 0
 
 	var numK     uint64 = 0
-	var numV     uint64 = 0
-	var minK     uint64 = MaxUint
-	var maxK     uint64 = 0
-	var minC     uint8  = 254
-	var maxC     uint8  = 0
-	var minD     uint64 = MaxUint
-	var maxD     uint64 = 0
-	var sumC     uint64 = 0
-	var sumD     uint64 = 0
 
 	succes = kio.ReadUint64(&regs)
 	if !succes { log.Panic("error reading begining of the file") }
@@ -958,19 +1003,9 @@ func (this *KmerHolder) FromFileHandle(inFh *xopen.Reader) bool {
 			}
 		}
 
-		numV++
-
 		kmer          = lastKmer + kmerdiff
 
-		if kmer     < minK { minK = kmer     }
-		if kmer     > maxK { maxK = kmer     }
-		if count    < minC { minC = count    }
-		if count    > maxC { maxC = count    }
-		if kmerdiff < minD { minD = kmerdiff }
-		if kmerdiff > maxD { maxD = kmerdiff }
-		
-		sumC += uint64(count)
-		sumD += kmerdiff
+		csk.Add(kmer, count, kmerdiff)
 		
 		//fmt.Printf("R k %d kmer %d count %d kmerdiff %d\n", numK, kmer, count, kmerdiff)
 		
@@ -981,57 +1016,16 @@ func (this *KmerHolder) FromFileHandle(inFh *xopen.Reader) bool {
 		}
 	}
 
-	fmt.Printf("READ  registers: %12d valid: %12d :: kmer min: %12d max: %12d :: diff sum: %12d min: %12d max: %12d :: count sum: %12d min: %12d max: %12d\n",
-		   numK, numV, minK, maxK, sumD, minD, maxD, sumC, minC, maxC )
+	fmt.Printf("READ  registers: %12d ", numK)
+	csk.Print()
 	
 	if numK != regs { log.Panicf("number of read registers not the same as expected. %d vs %d", numK, regs) }
-	
-	var numVc    uint64 = 0
-	var minKc    uint64 = MaxUint
-	var maxKc    uint64 = 0
-	var minCc    uint8  = 254
-	var maxCc    uint8  = 0
-	var minDc    uint64 = MaxUint
-	var maxDc    uint64 = 0
-	var sumCc    uint64 = 0
-	var sumDc    uint64 = 0
 
-	
-	succes = kio.ReadUint64(&numVc)
-	if !succes { log.Panic("error reading end of the file") }
-	if numV != numVc { log.Panicf("number of valid kmer not the same as expected. %d vs %d", numV, numVc) }
-	
-	succes = kio.ReadUint64(&minKc)
-	if !succes { log.Panic("error reading end of the file") }
-	if minK != minKc { log.Panicf("minimal kmer not the same as expected. %d vs %d", minK, minKc) }
-	
-	succes = kio.ReadUint64(&maxKc)
-	if !succes { log.Panic("error reading end of the file") }
-	if maxK != maxKc { log.Panicf("maximum kmer not the same as expected. %d vs %d", maxK, maxKc) }
-	
-	succes = kio.ReadUint8(&minCc)
-	if !succes { log.Panic("error reading end of the file") }
-	if minC != minCc { log.Panicf("minimal kmer not the same as expected. %d vs %d", minC, minCc) }
-	
-	succes = kio.ReadUint8(&maxCc)
-	if !succes { log.Panic("error reading end of the file") }
-	if maxC != maxCc { log.Panicf("maximum kmer not the same as expected. %d vs %d", maxC, maxCc) }
+	cskC := NewChecksumK()
 
-	succes = kio.ReadUint64(&minDc)
-	if !succes { log.Panic("error reading end of the file") }
-	if minD != minDc { log.Panicf("minimal kmer diff not the same as expected. %d vs %d", minD, minDc) }
-	
-	succes = kio.ReadUint64(&maxDc)
-	if !succes { log.Panic("error reading end of the file") }
-	if maxD != maxDc { log.Panicf("maximum kmer diff not the same as expected. %d vs %d", maxD, maxDc) }
+	kio.ReadStruct(cskC)
 
-	succes = kio.ReadUint64(&sumCc)
-	if !succes { log.Panic("error reading end of the file") }
-	if sumC != sumCc { log.Panicf("sum of counts not the same as expected. %d vs %d", sumC, sumCc) }
-
-	succes = kio.ReadUint64(&sumDc)
-	if !succes { log.Panic("error reading end of the file") }
-	if sumD != sumDc { log.Panicf("sum of diff not the same as expected. %d vs %d", sumD, sumDc) }
+	csk.IsEqual(cskC)
 		
 	return false
 }
